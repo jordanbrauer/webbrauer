3ab4d9930922e435b638dd5edab64ebf980f424d
jordanbrauer <jbrauer.inc@gmail.com>
2016-11-08 21:34:23 -0600

    Added build and watch tasks.

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,31 @@
-/** gulpfile.js
- */
+// gulpfile.js
 
 // gulp plugins
 const gulp = require('gulp');
+const del = require('del');
+const sass = require('gulp-sass');
+
+// variables
+// ...
+
+// clean task
+gulp.task('clean', () => del('./css/*.css'));
+
+// compile sass task
+gulp.task('compile', ['clean'], () => {
+  return gulp.src('./webbrauer.scss')
+    .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
+
+// batch tasks
+gulp.task('sass', ['clean', 'compile']);
+gulp.task('build', ['sass']);
+
+// watch task
+gulp.task('watch', ['build'], () => gulp.watch('./sass/**/*', ['build']));
 
 // default task
 gulp.task('default', () => {
-  console.log('testing... gulp is working! :)');
+  console.log('testing...');
 });
